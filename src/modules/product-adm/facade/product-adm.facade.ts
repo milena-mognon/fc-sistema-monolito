3ceba9c1680fbf254bc01bc23fe0266d7eec8ad1
milena-mognon <milenamognon@gmail.com>
2023-01-15 17:34:13 -0300

    feat: product adm facade

---
+++ src/modules/product-adm/facade/product-adm.facade.ts
@@ -0,0 +1,32 @@
+import { UseCaseInterface } from "../../@shared/domain/usecase/usecase.interface";
+import {
+  AddProductFacadeInputDTO,
+  CheckStockFacadeInputDTO,
+  CheckStockFacadeOutputDTO,
+  ProductAdmFacadeInterface,
+} from "./product-adm.facede.interface";
+
+export interface UseCasesProps {
+  addProductUseCase: UseCaseInterface;
+  checkStockUseCase: UseCaseInterface;
+}
+export class ProductAdmFacade implements ProductAdmFacadeInterface {
+  private _addProductUseCase: UseCaseInterface;
+  private _checkStockUseCase: UseCaseInterface;
+
+  constructor(usecasesProps: UseCasesProps) {
+    this._addProductUseCase = usecasesProps.addProductUseCase;
+    this._checkStockUseCase = usecasesProps.checkStockUseCase;
+  }
+
+  addProduct(input: AddProductFacadeInputDTO): Promise<void> {
+    // caso o dto do caso de uso for diferente do dto da facade, é necessário converter o dto da facade para o dto do cado se uso
+    return this._addProductUseCase.execute(input);
+  }
+
+  checkStock(
+    input: CheckStockFacadeInputDTO
+  ): Promise<CheckStockFacadeOutputDTO> {
+    return this._checkStockUseCase.execute(input);
+  }
+}
