56d6ec0e9e81d3ee001aff3ec91373b14d30bd6c
milena-mognon <milenamognon@gmail.com>
2023-01-21 18:49:32 -0300

    feat: check stock usecase and test

--- src/modules/product-adm/factory/facade.factory.ts
+++ src/modules/product-adm/factory/facade.factory.ts
@@ -1,15 +1,17 @@
 import { ProductAdmFacade } from "../facade/product-adm.facade";
 import { ProductRepository } from "../repository/product.repository";
 import { AddProductUseCase } from "../usecase/add-product/add-product.usecase";
+import { CheckStockUseCase } from "../usecase/check-stock/check-stock.usecase";
 
 export class ProductAdmFacadeFactory {
   static create() {
     const productRepository = new ProductRepository();
     const addProductUseCase = new AddProductUseCase(productRepository);
+    const checkStockUseCase = new CheckStockUseCase(productRepository);
 
     const productFacade = new ProductAdmFacade({
       addProductUseCase: addProductUseCase,
-      checkStockUseCase: undefined,
+      checkStockUseCase: checkStockUseCase,
     });
 
     return productFacade;
