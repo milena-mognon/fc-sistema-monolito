3a7982f349638a8c94d109a3990e39e55dea6dde
milena-mognon <milenamognon@gmail.com>
2023-01-29 15:13:18 -0300

    feat: find client facde, test and factory

--- src/modules/client-adm/facade/client-adm.facade.spec.ts
+++ src/modules/client-adm/facade/client-adm.facade.spec.ts
@@ -3,6 +3,10 @@ import { ClientModel } from "../repository/client.model";
 import { ClientRepository } from "../repository/client.repository";
 import { AddClientUseCase } from "../usecase/add-client/add-client.usecase";
 import { ClientAdmFacade } from "./client-adm.facade";
+import { FindClientUseCase } from "../usecase/find-client/find-client.usecase";
+import { Client } from "../domain/client.entity";
+import { Id } from "../../@shared/domain/value-object/id.value-object";
+import { ClientAdmFacadeFactory } from "../factory/facade.factory";
 
 describe("Client Adm Facade test", () => {
   let sequelize: Sequelize;
@@ -24,12 +28,7 @@ describe("Client Adm Facade test", () => {
   });
 
   it("should create a client", async () => {
-    const repository = new ClientRepository();
-    const addClientUseCase = new AddClientUseCase(repository);
-    const clientFacade = new ClientAdmFacade({
-      addClientUseCase: addClientUseCase,
-      findClientUseCase: undefined,
-    });
+    const facade = ClientAdmFacadeFactory.create();
 
     const input = {
       id: "1",
@@ -38,7 +37,7 @@ describe("Client Adm Facade test", () => {
       address: "Address 1",
     };
 
-    await clientFacade.add(input);
+    await facade.add(input);
 
     const client = await ClientModel.findOne({ where: { id: "1" } });
 
@@ -46,5 +45,28 @@ describe("Client Adm Facade test", () => {
     expect(client.name).toBe(input.name);
     expect(client.email).toBe(input.email);
     expect(client.address).toBe(input.address);
+  });
+
+  it("should find a client", async () => {
+    const input = {
+      id: "1",
+      name: "Client 1",
+      email: "client@mail.com",
+      address: "Street 1, 1111, City y",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    await ClientModel.create(input);
+
+    const facade = ClientAdmFacadeFactory.create();
+    const output = await facade.find({ id: "1" });
+
+    expect(output).toBeDefined();
+    expect(output.name).toBe(input.name);
+    expect(output.email).toBe(input.email);
+    expect(output.address).toBe(input.address);
+    expect(output.createdAt).toStrictEqual(input.createdAt);
+    expect(output.updatedAt).toStrictEqual(input.updatedAt);
   });
 });
