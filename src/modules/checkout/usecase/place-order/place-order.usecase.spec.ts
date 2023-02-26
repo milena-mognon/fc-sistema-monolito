72f3d63bd57e4638c188412604838bfe19257134
milena-mognon <milenamognon@gmail.com>
2023-02-26 15:36:21 -0300

    test: checkout - validate product stock

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -2,6 +2,65 @@ import { PlaceOrderUseCase } from "./place-order.usecase";
 import { PlaceOrderInputDTO } from "./place-order.usecase.dto";
 
 describe("Place Order usecase unit test", () => {
+  describe("Validate Products Method", () => {
+    //@ts-expect-error - no params in constructor
+    const placeOrderUseCase = new PlaceOrderUseCase();
+
+    it("should throw an error if no products are selected", async () => {
+      const input: PlaceOrderInputDTO = {
+        clientId: "0",
+        products: [],
+      };
+
+      await expect(
+        placeOrderUseCase["validateProducts"](input)
+      ).rejects.toThrow(new Error("No Products selected"));
+    });
+
+    it("should throw an error when product is out of stock", async () => {
+      const mockProductFacade = {
+        checkStock: jest.fn(({ productId }: { productId: string }) =>
+          Promise.resolve({
+            productId,
+            stock: productId === "1" ? 0 : 1,
+          })
+        ),
+      };
+
+      //@ts-expect-error - force set productFacade
+      placeOrderUseCase["_productFacade"] = mockProductFacade;
+
+      let input: PlaceOrderInputDTO = {
+        clientId: "0",
+        products: [{ productId: "1" }],
+      };
+
+      await expect(
+        placeOrderUseCase["validateProducts"](input)
+      ).rejects.toThrow(new Error("Product 1 is not available in stock"));
+
+      input = {
+        clientId: "0",
+        products: [{ productId: "0" }, { productId: "1" }],
+      };
+
+      await expect(
+        placeOrderUseCase["validateProducts"](input)
+      ).rejects.toThrow(new Error("Product 1 is not available in stock"));
+      expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(3);
+
+      input = {
+        clientId: "0",
+        products: [{ productId: "0" }, { productId: "1" }, { productId: "1" }],
+      };
+
+      await expect(
+        placeOrderUseCase["validateProducts"](input)
+      ).rejects.toThrow(new Error("Product 1 is not available in stock"));
+      expect(mockProductFacade.checkStock).toHaveBeenCalledTimes(5);
+    });
+  });
+
   describe("execute method", () => {
     it("should throw an error when client not found", async () => {
       const mockClientFacade = {
